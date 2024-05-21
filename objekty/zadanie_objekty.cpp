ddff6028b027ae8a167ce8382b982c5c36440f02
Bartek <bartoszwiadernylodz@gmail.com>
2024-05-21 18:41:33 +0200

    dodanie fuel car i electricar i metode jedz

--- objekty/zadanie_objekty.cpp
+++ objekty/zadanie_objekty.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 class Car{
     public:
@@ -6,17 +7,18 @@ class Car{
         string model;
         string kolor;
         int rok_produkcji;
+        int pojemnosc;
     protected:
         string vin;
     public:
         void dodaj_samochod();
         void pokaz_samochod();
-        void jedz(int km);
+        void jedz(int ile_km);
         void dodaj_vin();
         void pokaz_vin();
 };
 
-class FuelCar{
+class FuelCar: public Car{
     public:
         int pojemnosc_baku=0;
         int stan_baku;
@@ -24,9 +26,21 @@ class FuelCar{
     public:
         void pokaz_stan_baku();
         void zatankuj();
+        void jedz(int ile_km);
 };
+
+class ElecricCar: public Car{
+    public:
+        int poziom_baterii;
+        //string poziom_zu¿ycia_baterii;
+    public:
+        void laduj_baterie();
+        void  pokaz_poziom_naladowania_baterii();
+        void jedz(int ile_km);
+};
+
 void Car::dodaj_samochod(){
-    cout<<"----dodaj samochod----";
+    cout<<"\n----dodaj samochod----";
     cout<<"\npodaj marke:";
     cin>>marka;
     cout<<"podaj model:";
@@ -35,10 +49,13 @@ void Car::dodaj_samochod(){
     cin>>kolor;
     cout<<"podaj rok produkcji:";
     cin>>rok_produkcji;
+    cout<<"podaj pojemnosc:";
+    cin>>pojemnosc;
+    this->pojemnosc=pojemnosc;
 }
 
 void Car::pokaz_samochod(){
-    cout<<"----samochod----";
+    cout<<"\n----samochod----";
     cout<<"\nmarka:"<<this->marka;
     cout<<"\nmodel:"<<this->model;
     cout<<"\nkolor:"<<this->kolor;
@@ -57,30 +74,76 @@ void Car::pokaz_vin(){
 }
 
 void FuelCar::zatankuj(){
-    int ile,wynik=0;
+    int ile,wynik;
     cout<<"\n----zatankuj----";
+    cout<<"\nstan bak przed zatankowaniem:"<<this->stan_baku<<" litrow";
     cout<<"\npodaj ile chcesz zatankowac:";
     cin>>ile;
-    //wynik=this->pojemnosc_baku+ile;
-    cout<<"\nstan baku po zatankowaniu:"<<wynik;
-    wynik=this->pojemnosc_baku;
+    wynik=this->stan_baku+ile;
+    this->stan_baku=wynik;
+    if(this->stan_baku<=this->pojemnosc_baku){
+        cout<<"\nstan baku po zatankowaniu:"<<this->stan_baku<<" litrow";
+    }else{
+        cout<<"\nza duzo zatankowales!";
+    }
 }
 
 void FuelCar::pokaz_stan_baku(){
     cout<<"\n----stan baku----";
-    cout<<"\npojemnosc baku:"<<this->pojemnosc_baku;
-    this->spalanie="";
-    cout<<"\nspalanie:";
+    this->pojemnosc_baku=pojemnosc;
+    cout<<"\npojemnosc baku:"<<this->pojemnosc_baku<<" litrow";
+}
+
+void ElecricCar::laduj_baterie(){
+    int ile,wynik;
+    cout<<"\n----naladuj----";
+    cout<<"\npoziom baterii przed naladowaniem:"<<this->poziom_baterii<<" %";
+    cout<<"\npodaj ile chcesz naladowac:";
+    cin>>ile;
+    wynik=this->poziom_baterii+ile;
+    this->poziom_baterii=wynik;
+    cout<<"\nstan baku po naladowaniu:"<<this->poziom_baterii<<" %";
+
+}
+
+void ElecricCar::pokaz_poziom_naladowania_baterii(){
+    cout<<"\n----stan zuzycia baterii----";
+    this->poziom_baterii=pojemnosc;
+    cout<<"\npoziom baterii:"<<this->poziom_baterii<<" %";
+}
+
+
+void FuelCar::jedz(int ile_km){
+    double wynik;
+    cout<<"\n"<<this->stan_baku;
+    wynik=ile_km*(this->stan_baku/100);
+    cout<<"\n aktulany stan baku po przejechaniu "<<ile_km<<" wynosi:"<<wynik<<" Litrow";
+}
+
+void ElecricCar::jedz(int ile_km){
+    double wynik;
+    cout<<"\n"<<this->poziom_baterii;
+    wynik=ile_km*(this->poziom_baterii/100);
+    cout<<"\n aktulany stan baterii po przejechaniu "<<ile_km<<" wynosi:"<<wynik<<" %";
 }
 int main()
 {
-    Car car;
+    FuelCar car;
     car.dodaj_samochod();
     car.dodaj_vin();
     car.pokaz_samochod();
     car.pokaz_vin();
-    FuelCar fuelcar;
-    fuelcar.pokaz_stan_baku();
-    fuelcar.zatankuj();
+    car.pokaz_stan_baku();
+    car.zatankuj();
+    car.jedz(20);
+
+    ElecricCar car2;
+    car.dodaj_samochod();
+    car.dodaj_vin();
+    car.pokaz_samochod();
+    car.pokaz_vin();
+    car2.pokaz_poziom_naladowania_baterii();
+    car2.laduj_baterie();
+    car2.jedz(20);
     return 0;
 }
